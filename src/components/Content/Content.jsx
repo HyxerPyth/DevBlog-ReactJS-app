b9af5d7e7fa417318cf136ceb9215846d5ce04ac
Roman Loboda <hyawey2003@gmail.com>
2024-01-31 14:50:06 -0500

    Updated Content

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Content.module.css"
 import PostSelection from "./MyPosts/PostSelection/PostSeceltion";
 import { updatePostAreaActionCreator } from "../../redux/state"
@@ -7,6 +7,8 @@ import { NavLink } from "react-router-dom";
 
 
 const Content = (props) => {
+
+    let [ShowInstructions, setShowInstructions] = useState(false);
 
     let newPostElement = React.createRef(); 
 
@@ -21,12 +23,23 @@ const Content = (props) => {
         props.dispatch(action)
     }
 
+    let onClickInstructions = () => {
+        setShowInstructions(true);
+    }
+
     return (
         <div className={style.content}>
             <div>
-                <textarea className={style.textArea} onChange={ updatePostArea } value={ props.state.updatePostArea } ref={ newPostElement } >
+                <textarea className={style.textArea} onChange={ updatePostArea } onClick={ onClickInstructions } value={ props.state.updatePostArea } ref={ newPostElement } >
                   Type here...
                 </textarea>
+                <div>
+                    {ShowInstructions && (
+                        <div>
+                            <p className={style.instructions}>Hello!</p>
+                        </div>
+                    )}
+                </div>
             </div>
             <div>
               <button className={style.button} onClick={ AddPost } > Post </button>
